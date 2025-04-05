d200cf745e1a6545d17a26bcaf74ee40a8cb7356
kritoke <3081486+kritoke@users.noreply.github.com>
2025-04-05 16:41:01 -0500

    adjust output txt and add arugments

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from stats import count_words
+import sys
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    book_path = sys.argv[1]
     file_contents = read_text(book_path)
     word_count = count_words(file_contents)
     print(f"--- Begin report of {book_path} ---")
     print(f"{word_count} words found in the document")
     letter_count = count_chars(file_contents)
     for letter in letter_count:
-        print(f"The '{letter}' character was found {letter_count[letter]} times")
+        print(f"{letter}: {letter_count[letter]}")
     print("--- End report ---")
 
 def read_text(file_loc):
@@ -23,4 +24,8 @@ def count_chars(text):
             dictionary[letter] = lowered_string.count(letter)
     return dictionary
 
-main()
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main()
